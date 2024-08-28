2e66520e20c3a748f5fdd22e8fd0a6f2d427ae23
Delya Ardiyanti <adellyaa04@gmail.com>
2024-08-28 16:39:30 +0700

    update allowed_hosts

--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-pnmw&dw!g)e4jo$kcpzpskxmwavmbqeh+(cb!itz&(f^$nko%d
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "delya-ardiyanti-mentalhealthtracker.pbp.cs.ui.ac.id."]
 
 
 # Application definition
